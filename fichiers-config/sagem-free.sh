7747a6308773d2ee6d740f2959ae7af3ffeb60e8
MaximeALLAN <allan_m@etna-alternance.net>
2017-01-19 17:42:45 +0100

    nettoyage du dossier fichiers-config

--- fichiers-config/sagem-free.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-# /etc/init.d/sagem-free.sh
-# Script qui établit la connexion ADSL à Free via le modem SAGEM USB
-# Formation Debian GNU/Linux par Alexis de Lattre
-# http://formation-debian.via.ecp.fr/
-
-echo -n "Starting the connexion to Free via the SAGEM modem:"
-/usr/sbin/startadsl 1> /dev/null 2>&1
-echo " done."
